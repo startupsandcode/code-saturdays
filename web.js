9a4b79e0d0021240644b21213341dabc19996822
Nadim Jrab <nadjrab@Donias-MacBook-Air.local>
2020-01-11 16:42:04 -0500

    v

--- web.js
+++ web.js
@@ -1,22 +1,9 @@
 var http = require('http');
 
-<<<<<<< HEAD
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World\n');
 }).listen(8080);
 
-=======
-http.createServer(function (request, response) {
-   // Send the HTTP header 
-   // HTTP Status: 200 : OK
-   // Content Type: text/plain
-   response.writeHead(200, {'Content-Type': 'text/plain'});
-   
-   // Send the response body as "Hello World"
-   response.end('Hello World\n');
-}).listen(8080);
-
 // Console will print the message
->>>>>>> 7f4aca82007d5cfa5700d0f3296fc23be9201ea1
 console.log('Server running at http://127.0.0.1:8080/');

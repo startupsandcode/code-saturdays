1f7c860f47b44d798b10075d455fb9c9ec79c12a
John Mann <me@johnmann.org>
2020-01-25 08:33:37 -0500

    added simple sass

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,9 @@
 from flask import Flask
+from flask_scss import Scss
 
 app = Flask(__name__)
+
+app.env = 'DEVELOPMENT'
+Scss(app)
 
 from app import routes
--- microblog.py
+++ microblog.py
@@ -1,1 +1,4 @@
 from app import app
+
+if __name__ == "__main__":
+	app.run(debug=app.debug, port=5000, threaded=True)
